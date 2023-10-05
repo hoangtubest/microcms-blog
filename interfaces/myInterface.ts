81899d60d0b5ec50f094c9e223a21533ba22239a
hoangtubest <hoangtubest@gmail.com>
2023-10-05 13:35:39 +0700

    Update myInterface.ts

--- interfaces/myInterface.ts
+++ interfaces/myInterface.ts
@@ -1,27 +1,25 @@
-// // インターフェース
-
-// type useMicroCMSGetList = <T>(
-//   args: {
-//     endpoint: string;
-//     queries?: MicroCMSQueries;
-//   },
-//   fetchOptions?: FetchOptions
-// ) => Promise<AsyncData<MicroCMSListResponse<T>>>;
-// type useMicroCMSGetListDetail = <T>(
-//   args: {
-//     endpoint: string;
-//     contentId: string;
-//     queries?: MicroCMSQueries;
-//   },
-//   fetchOptions?: FetchOptions
-// ) => Promise<AsyncData<T & MicroCMSListContent>>;
-// type useMicroCMSGetObject = <T>(
-//   args: {
-//     endpoint: string;
-//     queries?: MicroCMSQueries;
-//   },
-//   fetchOptions?: FetchOptions
-// ) => Promise<AsyncData<T & MicroCMSObjectContent>>;
+type useMicroCMSGetList = <T>(
+  args: {
+    endpoint: string;
+    queries?: MicroCMSQueries;
+  },
+  fetchOptions?: FetchOptions
+) => Promise<AsyncData<MicroCMSListResponse<T>>>;
+type useMicroCMSGetListDetail = <T>(
+  args: {
+    endpoint: string;
+    contentId: string;
+    queries?: MicroCMSQueries;
+  },
+  fetchOptions?: FetchOptions
+) => Promise<AsyncData<T & MicroCMSListContent>>;
+type useMicroCMSGetObject = <T>(
+  args: {
+    endpoint: string;
+    queries?: MicroCMSQueries;
+  },
+  fetchOptions?: FetchOptions
+) => Promise<AsyncData<T & MicroCMSObjectContent>>;
 
 // // FetchOptions is the same as the second argument option of useFetch provided by Nuxt3.
 // // AsyncData is the return value of useFetch.
